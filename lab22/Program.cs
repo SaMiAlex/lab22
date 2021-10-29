4338a69d5f6630030b0b5735af0bcdde76e764cc
SaMiAlex <samias@mail.ru>
2021-10-29 12:48:13 +0600

    Добавьте файлы проекта.

---
+++ lab22/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lab22
+{
+    class Program
+    {
+        static int N;
+        static int[,] t;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сторону квадрата");
+            N = Convert.ToInt32(Console.ReadLine());
+            t = new int[N, N];
+            Random random = new Random();
+            #region//Формирование массива
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    t[i, j] = random.Next(10);
+                    Console.Write("{0} ", t[i, j]);
+                }
+                Console.WriteLine();
+            }
+            #endregion
+
+            Action action = new Action(calcSum);
+            Action<Task> actiontask = new Action<Task>(calcMax);
+            Task task1 = new Task(action);
+            Task task2 = task1.ContinueWith(actiontask);
+            task1.Start();            
+            Console.ReadKey();
+        }
+        static void calcSum()
+        {
+            int s = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    s += t[i, j];
+                    Thread.Sleep(10);
+                }
+            }
+            Console.WriteLine($"Сумма чисел массива равна {s}");
+        }
+        static void calcMax(Task task)
+        {
+            int s = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (t[i, j]>s)
+                    {
+                        s = t[i, j];
+                        Thread.Sleep(10);
+                    }                    
+                }
+            }
+            Console.WriteLine($"Максимальное число массива равно {s}");
+        }
+    }
+}
